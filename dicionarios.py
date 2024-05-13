51fd8ea3a89479cd3efc4efdf39ccc9e49d9d4b6
Duda Paschoal <dudapaschoal6@gmail.com>
2024-05-13 15:26:11 -0300

    Atualizando exemplos

--- dicionarios.py
+++ dicionarios.py
@@ -38,7 +38,7 @@ telefone = contatos["giovanna@gmail.com"]["telefone"]  # "3443-2121"
 print(telefone)
 
 extra = contatos["melaine@gmail.com"]["extra"]["a"]
-print(extra)
+print(extra) #1
 
     #Iterando Dicionários
 
@@ -56,4 +56,159 @@ print("=" * 100)
 
 for chave, valor in contatos.items():
     print(chave, valor)
+
+
+    #Clear limpar dicionario
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+contatos.clear()
+print(contatos)  # {}
+
+    #Copy  
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+copia = contatos.copy()
+copia["guilherme@gmail.com"] = {"nome": "Gui"}
+
+print(contatos["guilherme@gmail.com"])  # {"nome": "Guilherme", "telefone": "3333-2221"}
+
+print(copia["guilherme@gmail.com"])  # {"nome": "Gui"}
+
+    #Fromkeys cria chaves, quando quer criar chaves sem vincular valor, ou criar conjunto de chaves com valor padrao. 
+
+resultado = dict.fromkeys(["nome", "telefone"])  # {"nome": None, "telefone": None}
+print(resultado)
+
+resultado = dict.fromkeys(["nome", "telefone"], "vazio")  # {"nome": "vazio", "telefone": "vazio"}
+print(resultado)
+
+
+    #Get acessar valores dentro do dicionário, usa get para descobrir se existe uma chave
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+# contatos["chave"]  # KeyError
+
+resultado = contatos.get("chave")  # None
+print(resultado)
+
+resultado = contatos.get("chave", {})  # {}
+print(resultado)
+
+resultado = contatos.get(
+    "guilherme@gmail.com", {}
+)  # {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}
+print(resultado)
+
+    #.items
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.items()  # dict_items([('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})])
+print(resultado)
+
+    #keys retorna apenas as chaves
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.keys()  # dict_keys(['guilherme@gmail.com'])
+print(resultado)
+
+    #pop remove valores da chave e retorna o que removeu
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.pop("guilherme@gmail.com")  # {'nome': 'Guilherme', 'telefone': '3333-2221'}
+print(resultado)
+
+resultado = contatos.pop("guilherme@gmail.com", {})  # {}
+print(resultado)
+
+    #popitem nao informa a chave e retorna itens na sequencia
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+resultado = contatos.popitem()  # ('guilherme@gmail.com', {'nome': 'Guilherme', 'telefone': '3333-2221'})
+print(resultado)
+
+# contatos.popitem()  # KeyError
+
+    #setdefault se o atributo existir no dicionario apenas retorna e nao adiciona, se nao existir ele adiciona
+
+contato = {"nome": "Guilherme", "telefone": "3333-2221"}
+
+contato.setdefault("nome", "Giovanna")  # "Guilherme"
+print(contato)  ## {'nome': 'Guilherme', 'telefone': '3333-2221'}
+
+contato.setdefault("idade", 28)  # 28
+print(contato)  ## {'nome': 'Guilherme', 'telefone': '3333-2221', 'idade': 28}
+
+    #update ele deixa atualizar o dicionario 
+
+contatos = {"guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"}}
+
+contatos.update({"guilherme@gmail.com": {"nome": "Gui"}})
+print(contatos)  # {'guilherme@gmail.com': {'nome': 'Gui'}}
+
+contatos.update({"giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3322-8181"}})
+# {'guilherme@gmail.com': {'nome': 'Gui'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3322-8181'}}
+print(contatos)
+
+    #values vai retornar os valores apenas 
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+resultado = (
+    contatos.values()
+)  # dict_values([{'nome': 'Guilherme', 'telefone': '3333-2221'}, {'nome': 'Giovanna', 'telefone': '3443-2121'}, {'nome': 'Chappie', 'telefone': '3344-9871'}, {'nome': 'Melaine', 'telefone': '3333-7766'}])  # noqa
+print(resultado)
+
+
+    #in  
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+resultado = "guilherme@gmail.com" in contatos  # True
+print(resultado)
+
+resultado = "megui@gmail.com" in contatos  # False
+print(resultado)
+
+resultado = "idade" in contatos["guilherme@gmail.com"]  # False
+print(resultado)
+
+resultado = "telefone" in contatos["giovanna@gmail.com"]  # True
+print(resultado)
+
+    #del
+
+contatos = {
+    "guilherme@gmail.com": {"nome": "Guilherme", "telefone": "3333-2221"},
+    "giovanna@gmail.com": {"nome": "Giovanna", "telefone": "3443-2121"},
+    "chappie@gmail.com": {"nome": "Chappie", "telefone": "3344-9871"},
+    "melaine@gmail.com": {"nome": "Melaine", "telefone": "3333-7766"},
+}
+
+del contatos["guilherme@gmail.com"]["telefone"]
+del contatos["chappie@gmail.com"]
+
+# {'guilherme@gmail.com': {'nome': 'Guilherme'}, 'giovanna@gmail.com': {'nome': 'Giovanna', 'telefone': '3443-2121'}, 'melaine@gmail.com': {'nome': 'Melaine', 'telefone': '3333-7766'}}  # noqa
+print(contatos)
 
