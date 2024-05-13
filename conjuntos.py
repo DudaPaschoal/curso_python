10e5f65a62ec830910d03fff26e47cb77229d4d6
Duda Paschoal <dudapaschoal6@gmail.com>
2024-05-13 15:27:18 -0300

    Atualizando linhas

--- conjuntos.py
+++ conjuntos.py
@@ -77,3 +77,100 @@ print(resultado)
 
 resultado = conjunto_b.issubset(conjunto_a)  # False
 print(resultado)
+
+    #Issuperset ao contrario do issubset
+
+conjunto_a = {1, 2, 3}
+conjunto_b = {4, 1, 2, 5, 6, 3}
+
+resultado = conjunto_a.issuperset(conjunto_b)  # False
+print(resultado)
+
+resultado = conjunto_b.issuperset(conjunto_a)  # True
+print(resultado)
+
+    #isdisjoint - nenhum valor dentro do outro 
+
+conjunto_a = {1, 2, 3, 4, 5}
+conjunto_b = {6, 7, 8, 9}
+conjunto_c = {1, 0}
+
+resultado = conjunto_a.isdisjoint(conjunto_b)  # True
+print(resultado)
+
+resultado = conjunto_a.isdisjoint(conjunto_c)  # False
+print(resultado)
+
+    #add adicionar elemento
+
+sorteio = {1, 23}
+
+sorteio.add(25)  # {1, 23, 25}
+print(sorteio)
+
+sorteio.add(42)  # {1, 23, 25, 42}
+print(sorteio)
+
+sorteio.add(25)  # {1, 23, 25, 42}
+print(sorteio)
+
+    #clear 
+
+sorteio = {1, 23}
+
+print(sorteio)  # {1,23}
+
+sorteio.clear()
+
+print(sorteio)  # {}
+
+    #copy
+
+sorteio = {1, 23}
+
+print(sorteio)  # {1, 23}
+
+sorteio.copy()
+
+print(sorteio)  # {1, 23}
+
+    #discart -  descartar um valor
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+numeros.discard(1)
+numeros.discard(45)
+
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9, 0}
+
+    #pop 
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.pop())  # 0
+print(numeros.pop())  # 1
+print(numeros)  # {2, 3, 4, 5, 6, 7, 8, 9}
+
+    #remove - Da um erro se o elemento nao existe
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(numeros)  # {0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+print(numeros.remove(0))  # 0
+print(numeros)  # {1, 2, 3, 4, 5, 6, 7, 8, 9}
+
+    #len tamanho do conjunto
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(len(numeros))  # 10
+
+    #in
+
+numeros = {1, 2, 3, 1, 2, 4, 5, 5, 6, 7, 8, 9, 0}
+
+print(1 in numeros)  # True
+print(10 in numeros)  # False
