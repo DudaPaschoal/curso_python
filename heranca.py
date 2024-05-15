a94d3a0a09fa56cac0b013b5fefb3f5f66d153b5
Duda Paschoal <dudapaschoal6@gmail.com>
2024-05-15 11:10:42 -0300

    Aula sobre herancas, anotacoes

---
+++ heranca.py
@@ -0,0 +1,97 @@
+##Em programacao herenca é a capacidade de uma classe filha derivar ou herdar as características e comportamentos da classe pai(base)
+##Benefício representa bem os relacionamentos do mundo real
+##Fornece reutilizacao de código, nao  precisamos escrever o mesmo codigo repetidamente. Além disso permite adicionar mais recursos a uma classe sem modificá-la.
+##É de natureza transitiva, o que significa que, se a classe B herdar da classe A, todas as subclasses de B herdarao automaticamente da classe A. 
+
+#Exemplo sintaxe da heranca
+class A:
+    pass
+class B(A):
+    pass
+
+#Heranca múltipla quando uma classe filha herda de várias classes pai, ela é chamada de heranca múltipla
+#Exemplo
+class A:
+    pass
+class B:
+    pass
+class C(A, B):
+    pass
+
+#Exemplo heranca simples
+
+class Veiculo:
+    def __init__(self, cor, placa, numero_rodas):
+        self.cor = cor
+        self.placa = placa
+        self.numero_rodas = numero_rodas
+
+    def ligar_motor(self):
+        print("Ligando o motor")
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Motocicleta(Veiculo):
+    pass
+
+
+class Carro(Veiculo):
+    pass
+
+
+class Caminhao(Veiculo):
+    def __init__(self, cor, placa, numero_rodas, carregado):
+        super().__init__(cor, placa, numero_rodas)
+        self.carregado = carregado
+
+    def esta_carregado(self):
+        print(f"{'Sim' if self.carregado else 'Não'} estou carregado")
+
+
+moto = Motocicleta("preta", "abc-1234", 2)
+carro = Carro("branco", "xde-0098", 4)
+caminhao = Caminhao("roxo", "gfd-8712", 8, True)
+
+print(moto)
+print(carro)
+print(caminhao)
+
+
+#Heranca multipla
+
+class Animal:
+    def __init__(self, nro_patas):
+        self.nro_patas = nro_patas
+
+    def __str__(self):
+        return f"{self.__class__.__name__}: {', '.join([f'{chave}={valor}' for chave, valor in self.__dict__.items()])}"
+
+
+class Mamifero(Animal):
+    def __init__(self, cor_pelo, **kw):
+        self.cor_pelo = cor_pelo
+        super().__init__(**kw)
+
+
+class Ave(Animal):
+    def __init__(self, cor_bico, **kw):
+        self.cor_bico = cor_bico
+        super().__init__(**kw)
+
+
+class Gato(Mamifero):
+    pass
+
+
+class Ornitorrinco(Mamifero, Ave):
+    def __init__(self, cor_bico, cor_pelo, nro_patas):
+        super().__init__(cor_pelo=cor_pelo, cor_bico=cor_bico, nro_patas=nro_patas)
+
+
+gato = Gato(nro_patas=4, cor_pelo="Preto")
+print(gato)
+
+ornitorrinco = Ornitorrinco(nro_patas=2, cor_pelo="vermelho", cor_bico="laranja")
+print(ornitorrinco)
